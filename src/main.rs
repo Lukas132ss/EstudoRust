890e229ce9867434c3804e788fa726fad233fbd3
Lukas de Souza Santos <lk@serrante.com.br>
2025-08-19 10:23:26 -0300

    feat: Soma de digitos usando while

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,17 @@ fn convert_to_int(data_input: & String) -> i32 {
 }
 
 
+
 fn main() {
-    let mut number1 = String::new();
-    io::stdin().read_line(&mut number1).expect("Failed to read line");
-
-    let mut number2 = String::new();
-    io::stdin().read_line(&mut number2).expect("Failed to read line");
-
-    if convert_to_int(&number1) > convert_to_int(&number2) {
-        println!("Number {} greater than Number {}", convert_to_int(&number1), convert_to_int(&number2));
-    }else {
-        println!("Number {} less than or equal to Number {}", convert_to_int(&number1), convert_to_int(&number2));
-    }   
+    let mut soma: i32 = 0;
+    let mut valor_entrada: String = String::new();
+    io::stdin().read_line(&mut valor_entrada).expect("Failed to read line");
+    let mut valor_i32: i32 = convert_to_int(&valor_entrada);
+    while valor_i32 != 0 {
+        let r = valor_i32 % 10;
+        soma = soma + r;
+        valor_i32 = valor_i32 / 10;
+    }
+    println!("Valor da soma dos digitos: {}", soma);
 }
 
